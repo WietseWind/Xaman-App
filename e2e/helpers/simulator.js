5cc8a2d02d7546c85b6aeedd18cd1eaf728fa6ef
Wietse Wind <mail@wietse.com>
2026-08-18 16:04:49 +0200

    e2e: dedicated local simulator, fix stale process name, target detox device explicitly

--- e2e/helpers/simulator.js
+++ e2e/helpers/simulator.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const ARTIFACTS_DIR = path.resolve(__dirname, '../artifacts');
 
-const startDeviceLogStream = () => {
+const startDeviceLogStream = (udid = 'booted') => {
     const logFile = `${ARTIFACTS_DIR}/simulator.log`;
 
     if (!existsSync(ARTIFACTS_DIR)) {
@@ -17,7 +17,7 @@ const startDeviceLogStream = () => {
 
     const logStream = createWriteStream(logFile);
 
-    const args = ['simctl', 'spawn', 'booted', 'log', 'stream', '--predicate', 'process == "XUMM"'];
+    const args = ['simctl', 'spawn', udid, 'log', 'stream', '--predicate', 'process == "Xaman"'];
     const proc = spawn('xcrun', args, { stdio: 'pipe' });
 
     proc.stdout.pipe(logStream);
